f99e5bbc0e022c6728f70d66e918fcb08f11a930
D0rpm3n33r <emiel.stierman@gmail.com>
2023-12-27 22:33:18 +0100

    Tawk to code aangepast, hotjar en analytics ook

--- components/technicalComponents/HeadComponent/HeadComponent.js
+++ components/technicalComponents/HeadComponent/HeadComponent.js
@@ -67,7 +67,7 @@ export default class HeadComponent extends Component {
 					(function(){
 					var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 					s1.async=true;
-					s1.src='https://tawk.to/chat/658c631607843602b805f95f/1him6646m';
+					s1.src='https://embed.tawk.to/658c631607843602b805f95f/1him6646m';
 					s1.charset='UTF-8';
 					s1.setAttribute('crossOrigin','*');
 					s0.parentNode.insertBefore(s1,s0);})();`,
